e18fec8de7b38ba2c9a3af6be1f7439f10f17c9b
jorge niglia <jorge.niglia@gmail.com>
2023-04-30 22:33:05 -0300

    format corrections

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -7,7 +7,7 @@ export const RandomFox = ({ image, alt }: Props): JSX.Element => {
   return (
     <img
       src={image}
-      className="h-auto col-span-6 col-start-4 my-4 border-2 border-yellow-800 rounded-lg shadow-lg justify-self-center shadow-yellow-900"
+      className="w-4/5 h-auto col-span-4 col-start-5 my-4 border-2 border-yellow-800 rounded-lg shadow-lg justify-self-center shadow-yellow-900"
       alt={alt}
     />
   );
